37007b5b98714e1205bfd9f6680ab5dbf79f6195
jvgarcia <jvgarcia.21@est.ucab.edu.ve>
2024-07-26 20:13:34 -0400

    Seccion 5 Clase 8

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,11 @@
+
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-add-character',
+
+  templateUrl: './add-character.component.html',
+  styleUrl: './add-character.component.css',
+
+})
+export class AddCharacterComponent { }
